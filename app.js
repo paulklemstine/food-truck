72e6cd54c214282f154aec996a9ef4218d8a6855
VICTUS\paulk <paulklemstine@gmail.com>
2025-04-26 20:20:59 -0500

    server starts

---
+++ app.js
@@ -0,0 +1,12 @@
+//start express
+const express = require('express')
+const app = express()
+const port = 3000
+const url='http://localhost:3000/'
+const message = `Server is running on port ${port}. Visit ${url} in your browser.`
+app.listen(port, () => console.log(message))
+
+//setup middleware
+app.use(express.json())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }));
