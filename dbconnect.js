0007f9670e923c6556ccd6203043abc981bfd827
VICTUS\paulk <paulklemstine@gmail.com>
2025-04-28 17:08:20 -0500

    Added index.html and admin.html

---
+++ dbconnect.js
@@ -0,0 +1,25 @@
+const {MongoClient, ObjectId} = require('mongodb')
+const {uri} = require('./secrets/mongodb.json')
+const client = new MongoClient(uri)
+const getCollection = async (dbName, collectionName) => {
+    await client.connect()
+    return client.db(dbName).collection(collectionName)
+
+}
+
+
+//testing db connection
+let collection = null
+const test = async () => {
+    if (!collection) collection = await getCollection('food-truck', 'menu')
+        console.log(await collection.findOne())
+    return collection
+
+    
+}
+
+test()
+
+
+
+module.exports = { getCollection, ObjectId }
