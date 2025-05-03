817520ae0155bd921034e62b93e752d05a847884
Haley Bridges <300093282@fvtc.edu>
2025-05-02 19:30:00 -0500

    Addede Post and endpoints

--- routes/api/v1/foodtruck.js
+++ routes/api/v1/foodtruck.js
@@ -27,4 +27,22 @@ router.get('/events', async (request,response)=>{
     else response.send({ error: { message: 'Could not find any events' }})
 })
 
+router.post('/menu/add' , async(request, response) => {
+    const { name, description, price, url } = request.body
+    const newMenuItem = { name, description, price, url}
+
+    const collection = await getMenu()
+    const result = await collection.insertOne(newMenuItem)
+    response.send(newMenuItem)
+})
+
+router.post('/events/add' , async(request, response) => {
+    const { name, location, date, time } = request.body
+    const newEventItem = { name, location, date, time}
+
+    const collection = await getEvents()
+    const result = await collection.insertOne(newEventItem)
+    response.send(newEventItem)
+})
+
 module.exports = router
